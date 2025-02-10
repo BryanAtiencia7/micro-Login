e8a6887ca81abc5cb1efe662173e759b239fba94
Bryan Atiencia DEV <baatiencia@uce.edu.ec>
2025-02-10 01:03:51 -0500

    fix: :sparkles: Login and JWT working

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token
+from dotenv import load_dotenv
+import os
+import pymysql
 import bcrypt
 
+load_dotenv()
+
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'tu_clave_secreta'
@@ -11,9 +19,14 @@ CORS(app)
 
 jwt = JWTManager(app)
 
-usuarios = {
-    "usuario1": bcrypt.hashpw("password123".encode('utf-8'), bcrypt.gensalt())
-}
+def obtener_conexion():
+    return pymysql.connect(
+        host=db_host, 
+        user=db_user, 
+        password=db_password, 
+        database='usuario_db',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -21,7 +34,15 @@ def login():
     username = data.get('username')
     password = data.get('password').encode('utf-8')
 
-    if username in usuarios and bcrypt.checkpw(password, usuarios[username]):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    cursor.execute("SELECT password FROM usuarios WHERE username = %s", (username,))
+    usuario = cursor.fetchone()
+
+    conexion.close()
+
+    if usuario and bcrypt.checkpw(password, usuario['password'].encode('utf-8')):
         access_token = create_access_token(identity=username)
         return jsonify(access_token=access_token), 200
     else:
